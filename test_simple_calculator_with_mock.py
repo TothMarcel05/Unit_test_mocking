0b1fdfe95fbd59f7d906c3aac159421a38f1e2d3
Tóth Marcel Dániel <toth.marcel.daniel@diak.szbi-pg.hu>
2026-01-19 11:55:28 +0100

    .

--- test_simple_calculator_with_mock.py
+++ test_simple_calculator_with_mock.py
@@ -1,8 +1,32 @@
 def test_add_with_mock_history_service():
-    pass
+        if self.validation_service:
+            self.validation_service.validate_numbers(1, 2)
+        
+        result = 1 + 2
+        
+        if self.logger:
+            self.logger.log_operation("add", 1, 2, result)
+        
+        if self.history_service:
+            self.history_service.save_operation("add", 1, 2, result)
+        
+        return result
 
 def test_multiply_with_multiple_mocks():
-    pass
+        if self.validation_service:
+        self.validation_service.validate_numbers(1, 2)
+        
+        result = 1 * 2
+        
+        if self.logger:
+            self.logger.log_operation("multiply", 1, 2, result)
+        
+        if self.history_service:
+            self.history_service.save_operation("multiply", 1, 2, result)
+        
+        return result
 
 def test_get_history_count_with_mock():
-    pass
+        if self.history_service:
+        return self.history_service.get_operation_count()
+        return 0
